3d8a4ca653864ac1c78b5faf6bb1527d06f01c3c
miyusuzuki <107457233+miyusuzuki@users.noreply.github.com>
2022-07-12 12:56:47 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,10 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
