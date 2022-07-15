90b236c1fe9ecd2f3ecadb9a21aae0962035ca94
miyusuzuki <107457233+miyusuzuki@users.noreply.github.com>
2022-07-15 11:24:11 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @user = User.find(params[:id])
-    @book = Book.find(params[:id])
+    @books = Book.where(user_id: @user.id)
     @book = Book.new
     @books = @user.books
   end
@@ -18,10 +18,27 @@ class UsersController < ApplicationController
     @user = current_user
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]="You have updated user successfully."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+    
+    
+  
 
 private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
+  def book_params
+    params.permit(:title, :opinion)    
   end
 
 end
